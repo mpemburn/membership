b6b6a8bea5b48f7ecab0bbce6b3417ceca964470
Mark Pemburn <mark@pemburn.com>
2021-03-02 10:42:37 -0500

    Install Vue.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,36 +1,32 @@
-import Comparator from "./comparator";
-import RequestAjax from "./request-ajax";
-import Modal from './modal';
-import Confirmation from './confirmation';
-import DatatablesManager from './datatables-manager';
-import PermissionsManager from './permissions-manager';
-import UserRolesManager from './user-roles-manager';
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
 
-window.$ = require('jquery');
 require('./bootstrap');
-require('alpinejs');
-require('datatables.net')
 
-let ajax = new RequestAjax();
-let comparator = new Comparator();
-let modal = new Modal();
-let confirmation = new Confirmation();
-let dtManager = new DatatablesManager();
+window.Vue = require('vue').default;
 
-new PermissionsManager({
-    comparator: comparator,
-    ajax: ajax,
-    modal: modal,
-    confirmation: confirmation,
-    dtManager: dtManager
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i)
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
 });
-
-new UserRolesManager({
-    comparator: comparator,
-    ajax: ajax,
-    modal: modal,
-    confirmation: confirmation,
-    dtManager: dtManager
-});
-
-
